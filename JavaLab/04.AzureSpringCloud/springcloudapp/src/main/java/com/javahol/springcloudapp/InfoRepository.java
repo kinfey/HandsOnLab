9d562d8d79185143239e7793dc3efd0b7aac1e74
Kinfey <kinfeylo@microsoft.com>
2021-11-01 20:03:09 +0800

    init

--- JavaLab/04.AzureSpringCloud/springcloudapp/src/main/java/com/javahol/springcloudapp/InfoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.javahol.springcloudapp;
-
-
-import com.azure.spring.data.cosmos.repository.ReactiveCosmosRepository;
-
-public interface InfoRepository extends ReactiveCosmosRepository<Info,String> {
-    
-}
